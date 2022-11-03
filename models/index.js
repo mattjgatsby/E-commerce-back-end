41885e1c28345951de59b5088c295750b688a14c
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-11-03 19:48:42 -0400

    added delete route to products, working through bug with update route

--- models/index.js
+++ models/index.js
@@ -10,15 +10,15 @@ Product.belongsTo(Category, {
   onDelete: "CASCADE",
 });
 // Categories have many Products
-Category.hasMany(Product, { foreignKey: "category_id", });
+Category.hasMany(Product, { foreignKey: "category_id" });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag ,
+  through: ProductTag,
   foreignKey: "product_id",
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag ,
+  through: ProductTag,
   foreignKey: "tag_id",
 });
 module.exports = {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -48,7 +48,7 @@ router.post("/", (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds.length && req.body.tagIds) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -109,8 +109,22 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: "No product with that id found." });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
