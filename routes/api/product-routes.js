a2b662ec1fdae4c11128502c77e54603de1cb6b8
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-11-03 21:41:51 -0400

    updated product.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -11,6 +11,10 @@ router.get("/", async (req, res) => {
     const productData = await Product.findAll({
       include: [{ model: Category }, { model: Tag }],
     });
+    if (!productData) {
+      res.status(404).json({ message: "No product found" });
+      return;
+    }
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
